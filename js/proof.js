eb078b018b29da1fc9b4820181924aaa8a466966
Mei Rose Connor <meconnor@riley-tll.labs.stonybrook.edu>
2021-08-31 16:05:37 -0400

    make up to date

--- js/proof.js
+++ js/proof.js
@@ -1,11 +1,14 @@
 class Proof {
 
-  constructor(show, premises, save) {
+  constructor(show, premises=[], axioms=[],save) {
     this.save=save
     this.show = show
 
     this.premises = premises
-    this.lines = this.premises.map((premise) => new Line(premise, "premise", []));
+    this.axioms=axioms
+
+    this.axioms = this.axioms.map((axiom) => new Line(axiom, "axiom", []));
+    this.lines = [...this.axioms,...this.premises.map((premise) => new Line(premise, "premise", []))];
 
   }
 
@@ -20,14 +23,15 @@ class Proof {
   }
 
 checkForSolved(wff){
-  if(wff=this.show){
+  if(wff==this.show){
+    AnimateRotate(360,"#proof",()=>alert("Congratulations, you successfully proved the theorem!"))
     this.save.saveProof(this)
   }
 }
   deleteLine(uid) {
     var currentLine = this.lines.find(item => item.uid == uid)
     console.log(currentLine.justification)
-    if (currentLine.justification !== "premise") {
+    if (currentLine.justification !== "axiom") {
       this.lines = this.lines.filter(item => item.uid !== uid)
 
       var dependants = this.lines.filter((line) =>
